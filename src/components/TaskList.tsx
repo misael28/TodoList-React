99c80ffcfe9392f35a73c8d4dc98afcd005ad1a7
Souza <38145379+misael28@users.noreply.github.com>
2021-09-04 21:06:18 -0300

    feat: add remove task list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -33,6 +33,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList = tasks.filter((x) => x.id !== id);
+    setTasks(newTaskList);
   }
 
   return (
